f95fa4c107b5ce4caa6ca94ca0b02c463def94e1
Bernard <gicehajunior76@gmail.com>
2020-05-11 20:09:43 +0300

    opencv python-human detection

---
+++ face-detect.py
@@ -0,0 +1,38 @@
+import cv2
+import sys
+
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+
+# capture video using cv2
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # capture frame by frame, i.e, one by one 
+    ret, frame = video_capture.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # for each face on the projected on the frame
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.1,
+        minNeighbors = 5, 
+        # minSize(35, 35)
+    )
+    
+    # loop through the video faces for detection
+    for (x, y, w, h) in faces:
+        point1 = x+w
+        point2 = y+h
+        frame_color = (50, 50, 200)
+        
+        cv2.rectangle(frame, (x, y), (point1, point2), frame_color, 2)
+        cv2.imshow('video', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            sys.exit()
+        
+
+
+
+
